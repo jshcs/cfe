0c2fdc1181f60f6719ec78a06f54dbef4d7b2dbe
jshcs <jshcs@users.noreply.github.com>
2018-03-08 04:36:16 -0500

    refactored/replaced with in-built methods

--- src/features/featureExtraction.py
+++ src/features/featureExtraction.py
@@ -1,29 +1,14 @@
-
 def is_all_caps(token):
-    for t in token:
-        if t>'Z' or t<'A':
-            return False
-
-    return True
+    return token.isupper()
 
 def is_alpha_num(token):
-    for t in token:
-        if (t<='z' and t>='a') or (t<='Z' and t>='A') or (t<='9' and t>='0'):
-            continue
-        else:
-            return False
-
-    return True
+    return token.isalnum()
 
 def word_length(token):
     return len(token)
 
 def is_number(token):
-    for t in token:
-        if t>'9' or t<'0':
-            return False
-
-    return True
+    return token.isdigit()
 
 def ends_with_period(token):
     return token[-1]=='.'
