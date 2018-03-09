4ba2fabef2f29e4878af2f0ecb4919b1998771a3
jshcs <jshcs@users.noreply.github.com>
2018-03-09 03:30:54 -0500

    Sample output added as a comment

--- src/features/main.py
+++ src/features/main.py
@@ -5,3 +5,8 @@ token="1234"
 t=Features(token)
 
 print t.get_features()
+
+
+'''
+{'first_name_lexicon': False, 'is_alpha_num': True, 'is_number': True, 'last_name_lexicon': False, 'word_length': 4, 'is_all_caps': False, 'ends_with_period': False}
+'''
