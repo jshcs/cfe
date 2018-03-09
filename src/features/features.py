d13a9fe5ba63f6f84e189e04099228a4218b3f07
jshcs <jshcs@users.noreply.github.com>
2018-03-09 03:27:16 -0500

    Features class added, data files added, other minor refactoring

---
+++ src/features/features.py
@@ -0,0 +1,46 @@
+from config import *
+from utils import *
+
+class Features():
+	def __init__(self,token):
+		self.token=token
+		self.features={k:None for k in feature_names}
+
+	def is_all_caps(self):
+		self.features["is_all_caps"]=self.token.isupper()
+
+	def is_alpha_num(self):
+		self.features["is_alpha_num"]=self.token.isalnum()
+
+	def word_length(self):
+		self.features["word_length"]=len(self.token)
+
+	def is_number(self):
+		self.features["is_number"]=self.token.isdigit()
+
+	def ends_with_period(self):
+		self.features["ends_with_period"]=self.token[-1]=='.'
+
+	def first_name_lexicon(self):
+		arr=read_sorted_file_into_array(SORTED_FPERSON_FNAME)
+		start=0
+		end=len(arr)-1
+		self.features["first_name_lexicon"]=binary_search(arr,self.token.upper(),start,end)
+
+	def last_name_lexicon(self):
+		arr=read_sorted_file_into_array(SORTED_LPERSON_FNAME)
+		start=0
+		end=len(arr)-1
+		self.features["last_name_lexicon"]=binary_search(arr,self.token.upper(),start,end)
+
+	def get_features(self):
+		self.is_all_caps()
+		self.is_alpha_num()
+		self.word_length()
+		self.is_number()
+		self.ends_with_period()
+		self.first_name_lexicon()
+		self.last_name_lexicon()
+		return self.features
+
+
---
+++ src/features/main.py
@@ -0,0 +1,7 @@
+from features import *
+
+token="1234"
+
+t=Features(token)
+
+print t.get_features()
