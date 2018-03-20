97837a472d5516b615dac8454120eff81211a5ff
jshcs <jshcs@users.noreply.github.com>
2018-03-19 23:23:55 -0400

    sample output added

--- src/features/main.py
+++ src/features/main.py
@@ -1,6 +1,6 @@
 from features import *
 
-token="vo:l."
+token="[10--20]"
 
 t=Features(token)
 f=t.get_features()
@@ -14,12 +14,12 @@ print
 print f
 
 '''
-Total features: 9
+Total features: 14
 
-['first_name_lexicon', 'is_alpha_num', 'is_capitalized', 'last_name_lexicon', 'is_all_caps', 'word_length', 'enclosed_brackets', 'is_number', 'ends_with_period']
+['is_valid_year', 'first_name_lexicon', 'is_alpha_num', 'is_capitalized', 'last_name_lexicon', 'is_all_caps', 'is_etal', 'has_hyphen', 'word_length', 'enclosed_brackets', 'is_number', 'ends_with_period', 'has_colon', 'is_special_token']
 
 The key-value pairs are:
 
-{'first_name_lexicon': False, 'is_alpha_num': False, 'is_capitalized': False, 'last_name_lexicon': False, 'is_all_caps': False, 'word_length': 6, 'enclosed_brackets': True, 'is_number': False, 'ends_with_period': False}
+{'is_valid_year': False, 'first_name_lexicon': False, 'is_alpha_num': False, 'is_capitalized': False, 'last_name_lexicon': False, 'is_all_caps': False, 'is_etal': False, 'has_hyphen': True, 'word_length': 8, 'enclosed_brackets': True, 'is_number': False, 'ends_with_period': False, 'has_colon': False, 'is_special_token': False}
 
 '''
