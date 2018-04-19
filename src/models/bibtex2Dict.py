99d289b9f9b3f4f6271cbb7da021d26e67527161
KwanSY <32377701+KwanSY@users.noreply.github.com>
2018-04-19 19:18:00 -0400

    fix on labels
    fix on labels

--- src/models/bibtex2Dict.py
+++ src/models/bibtex2Dict.py
@@ -2,15 +2,13 @@ import numpy as np
 
 Bibtexlabels = {'author':0,'title':1,'journal':2,'year':3,'volume':4,'pages':5}
 
-data_dir = '../../data/'
-
 #input the name of style file, such as 'biochem'
 #output the train dictionary, val dictionary, test dictionary
 #dictionary[citation string][0] = list of tokens
 #dictionary[citation string][1] = index of label
 def dict_of_style(style):
-    strings = np.load(data_dir+'final_'+style+'_strings.npy')
-    pairs = np.load(data_dir+'final_'+style+'_pairs.npy')
+    strings = np.load('final_'+style+'_strings.npy')
+    pairs = np.load('final_'+style+'_pairs.npy')
     
     trainData = {}
     valData = {}
@@ -41,7 +39,7 @@ def dict_of_style(style):
         for j in range(len(pairs[i])):
             tokenList.append(pairs[i][j][0])
             if pairs[i][j][1] in Bibtexlabels:
-                labelList.append(labels[Bibtexlabels[i][j][1]])
+                labelList.append(Bibtexlabels[pairs[i][j][1]])
             else:
                 labelList.append(6)
         valData[strings[i]] = (tokenList,labelList)
@@ -52,7 +50,7 @@ def dict_of_style(style):
         for j in range(len(pairs[i])):
             tokenList.append(pairs[i][j][0])
             if pairs[i][j][1] in Bibtexlabels:
-                labelList.append(labels[Bibtexlabels[i][j][1]])
+                labelList.append(Bibtexlabels[pairs[i][j][1]])
             else:
                 labelList.append(6)
         testData[strings[i]] = (tokenList,labelList)
