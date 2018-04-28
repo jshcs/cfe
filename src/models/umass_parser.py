c12d8a2fd5665282ea090b5c755073841616cb84
Chesta Singh <32111784+csingh1024@users.noreply.github.com>
2018-04-28 07:41:15 -0400

    new api to fetch umass dataset

--- src/models/umass_parser.py
+++ src/models/umass_parser.py
@@ -4,6 +4,7 @@ import csv
 from config import *
 #from utils import read_file_into_array,read_sorted_file_into_array
 import random
+import pickle
 
 def read_sorted_file_into_array(filename):
     res=[]
@@ -28,8 +29,10 @@ def is_ascii(s):
 
 class GetDict():
     def __init__(self,filename):
+        self.filename = filename
         with open(filename,'r') as f:
             self.content=f.readlines()
+        self.file_dict= {TRAIN_FILE : '../../data/umass_train_data', TEST_FILE : '../../data/umass_test_data' , DEV_FILE :'../../data/umass_val_data'}
         self.labels=[]
         self.citation_strings=[]
         self.token_label={}
@@ -37,7 +40,47 @@ class GetDict():
         self.spl={'&':'Rand','"':'Rdquote',"'":'Rquote'}
         self.journal_names=read_sorted_file_into_array(SORTED_JNAMES)
         self.bio_titles=read_file_into_array(BIO_TITLES)
+        # to test if year is not proper
+        self.count_year_labels = []
+
+    def save_data(self, filename, X, Y , save_dict, labels):
+        z = raw_input("Do you want to overwrite the existing pickle files ? Y/N")
+        if z == 'Y' :
+            z = raw_input("Are u sure?")
+        if z!= 'Y':
+            print "not overwriting"
+            return
+        fname = self.file_dict[filename]
+        with open(fname + '.pkl', 'wb') as outp:
+            pickle.dump(X, outp)
+            pickle.dump(Y, outp)
+
+        with open(fname +'_dict.pkl', 'wb') as handle :
+            pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        with open(fname +'_labels_dict.pkl', 'wb') as handle :
+            pickle.dump(labels, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+    def get_all_saved_dict(self):
+        fname = self.file_dict[self.filename]
+
+        with open(fname + '.pkl', 'rb') as intp :
+            X = pickle.load(intp)
+            Y = pickle.load(intp)
+
+        with open(fname + '_dict.pkl', 'rb') as handle:
+            all_dicts = pickle.load(handle)
+
+        with open(fname +'_labels_dict.pkl', 'rb') as handle :
+            labels = pickle.load(handle)
+
+        return X,Y , all_dicts , labels
+
+
     def make_dict(self):
+        X = []
+        Y = []
         for line in self.content:
             curr_sentence=[]
             temp_dict={}
@@ -46,6 +89,8 @@ class GetDict():
             for spchar in self.spl.keys():
                 if spchar in mod_string:
                     mod_string=mod_string.replace(spchar,self.spl[spchar])
+            x = []
+            y = []
             tree_rec=ET.fromstring(mod_string,self.parser)
             for ele in tree_rec.iter():
                 if ele.text!=" ":
@@ -53,13 +98,11 @@ class GetDict():
                         if 'person' in ele.tag:
                             txt=ele.text.strip().split(" ")
                             txt=[t for t in txt if t not in PUNCT and len(t)>=1 and is_ascii(t)]
-                            #print txt
                             for t in txt:
-                                #if t not in PUNCT:
                                 temp_dict[t]='person'
                                 tmp_labels.append(labels['person'])
-                                # else:
-                                # 	print t
+                                x.append(t)
+                                y.append('author')
                         elif 'journal' in ele.tag:
                             txt=random.choice(self.journal_names).strip().split(" ")
                             #print txt
@@ -67,6 +110,8 @@ class GetDict():
                             for t in txt:
                                 temp_dict[t]='journal'
                                 tmp_labels.append(labels['journal'])
+                                x.append(t)
+                                y.append('journal')
                         elif 'title' in ele.tag:
                             txt=random.choice(self.bio_titles).strip().split(" ")
                             #print txt
@@ -74,6 +119,8 @@ class GetDict():
                             for t in txt:
                                 temp_dict[t]='title'
                                 tmp_labels.append(labels['title'])
+                                x.append(t)
+                                y.append('title')
                         else:
                             txt=ele.text.strip().split(" ")
                             txt=[t for t in txt if t not in PUNCT and len(t)>=1 and is_ascii(t)]
@@ -81,22 +128,33 @@ class GetDict():
                                 #if t not in PUNCT:
                                 temp_dict[t]=ele.tag
                                 if ele.tag not in labels:
-                                    tmp_labels.append(len(labels))
+                                    labels[ele.tag] = len(labels)
+                                    tmp_labels.append(labels[ele.tag])
+                                    x.append(t)
+                                    y.append(ele.tag)
                                 else:
                                     tmp_labels.append(labels[ele.tag])
+                                    x.append(t)
+                                    y.append(ele.tag)
                                 # else:
                                 # 	print t
                         curr_sentence=curr_sentence+txt
                         #print curr_sentence
-
+            X.append(x)
+            Y.append(y)
             self.citation_strings.append(' '.join(curr_sentence))
             self.labels.append(tmp_labels)
             self.token_label[self.citation_strings[-1]]=(curr_sentence,tmp_labels)
+        self.save_data(self.filename,X,Y,self.token_label, labels)
+
 
     def get_dict(self,citation_string):
         return self.token_label[citation_string]
+
     def get_all_dict(self):
+        print len(self.token_label)
         return self.token_label
+
     def print_all_dict(self):
         for s in self.citation_strings:
             print "Sentence"
@@ -113,11 +171,37 @@ class GetDict():
         return len(self.citation_strings)
 
 
-##test=GetDict(TRAIN_FILE)
-##test.make_dict()
-##test.print_all_dict()
-##print test.citation_strings[0]
-##print test.token_label[test.citation_strings[0]][0]
-##print test.token_label[test.citation_strings[0]][1]
-##print len(test.token_label[test.citation_strings[0]][0])
-##print test.get_size()
+'''
+test=GetDict(TEST_FILE)
+# Comment this , it will re write all existing file
+Z = test.make_dict()
+X, Y, dict , tlabels = test.get_all_saved_dict()
+keys = []
+count = 0
+print tlabels.keys()
+for x,y in zip(X,Y) :
+    sentence = ' '.join(x)
+    x1, y1 = dict[sentence]
+    for a,b,c,d in  zip(x,x1,y,y1):
+        if a!=b :
+            print zip(x,x1)
+            break
+        if c in tlabels.keys() and tlabels[c] == d :
+            continue
+        elif c == d :
+            continue
+        else :
+            print 'ele' ,c , d
+
+
+print len(test.count_year_labels)
+print test.count_year_labels
+print test.get_all_dict().items()
+print 'citation'
+print test.citation_strings
+print test.token_label[test.citation_strings[0]][0]
+print test.token_label[test.citation_strings[0]][1]
+print len(test.token_label[test.citation_strings[0]][0])
+print test.get_size()
+'''
+
