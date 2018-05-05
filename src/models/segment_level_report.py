671a8bd73d56fb68a52580a906552551375982c8
KwanSY <32377701+KwanSY@users.noreply.github.com>
2018-05-04 21:33:43 -0400

    updated with segment level precision, recall, f1
    updated with segment level precision, recall, f1 score

--- src/models/segment_level_report.py
+++ src/models/segment_level_report.py
@@ -3,6 +3,9 @@ from config import *
 from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 #load the ground truth and predicted result
 #y_test is the ground truth and y_pred is the predict result, both are in token level, have not reshape and squeeze
@@ -13,12 +16,14 @@ y_pred = np.load('final_model/test_result.npy')
 print y_test.shape
 print y_pred.shape
 
+
 #from one_hot to label
 y_test = np.argmax(y_test, axis = 2)
 y_pred = np.argmax(y_pred, axis = 2)
 
 print y_test.shape
 print y_pred.shape
+
 
 #transform token to segment, each citation string has 6 segment, start position and length in one segment
 #the format of segment is string '(pos,length)' for label
@@ -42,12 +47,14 @@ def get_segment(token_label):
         
     return seg_result[1:]
 
+
 #transform token level to segment level for ground truth and prediction
 y_seg_test = get_segment(y_test)
 y_seg_pred = get_segment(y_pred)
 
 print y_seg_test.shape
 print y_seg_pred.shape
+
 
 #f1 score for author
 person_truth = np.reshape(y_seg_test[:,0],(1,-1))
@@ -57,7 +64,11 @@ person_pred = np.squeeze(person_pred)
 #print 'person_truth',person_truth.shape,'person_pred',person_pred.shape
 
 f1_person = f1_score(person_truth,person_pred,average='micro')
-print 'author F1 score',f1_person
+prescision_person = precision_score(person_truth, person_pred, average='micro') 
+recall_person = recall_score(person_truth, person_pred, average='micro')  
+
+print 'author prescision', prescision_person,'recall', recall_person,'F1 score',f1_person
+
 
 #f1 score for title
 title_truth = np.reshape(y_seg_test[:,1],(1,-1))
@@ -67,7 +78,10 @@ title_pred = np.squeeze(title_pred)
 #print 'title_truth',title_truth.shape,'title_pred',title_pred.shape
 
 f1_title = f1_score(title_truth,title_pred,average='micro')
-print 'title F1 score',f1_title
+prescision_title = precision_score(title_truth, title_pred, average='micro') 
+recall_title = recall_score(title_truth, title_pred, average='micro')  
+
+print 'title prescision', prescision_title,'recall', recall_title,'F1 score',f1_title
 
 
 #f1 score for journal
@@ -78,7 +92,10 @@ journal_pred = np.squeeze(journal_pred)
 #print 'title_truth',journal_truth.shape,'title_pred',journal_pred.shape
 
 f1_journal = f1_score(journal_truth,journal_pred,average='micro')
-print 'journal F1 score',f1_journal
+prescision_journal = precision_score(journal_truth, journal_pred, average='micro') 
+recall_journal = recall_score(journal_truth, journal_pred, average='micro')  
+
+print 'journal prescision', prescision_journal,'recall', recall_journal,'F1 score',f1_journal
 
 
 #f1 score for year
@@ -89,7 +106,10 @@ year_pred = np.squeeze(year_pred)
 #print 'year_truth',year_truth.shape,'year_pred',year_pred.shape
 
 f1_year = f1_score(year_truth,year_pred,average='micro')
-print 'year F1 score',f1_year
+prescision_year = precision_score(year_truth, year_pred, average='micro') 
+recall_year = recall_score(year_truth, year_pred, average='micro')  
+
+print 'year prescision', prescision_year,'recall', recall_year,'F1 score',f1_year
 
 
 #f1 score for volume
@@ -100,7 +120,10 @@ volume_pred = np.squeeze(volume_pred)
 #print 'volume_truth',volume_truth.shape,'volume_pred',volume_pred.shape
 
 f1_volume = f1_score(volume_truth,volume_pred,average='micro')
-print 'volume F1 score',f1_volume
+prescision_volume = precision_score(volume_truth, volume_pred, average='micro') 
+recall_volume = recall_score(volume_truth, volume_pred, average='micro')  
+
+print 'volume prescision', prescision_volume,'recall', recall_volume,'F1 score',f1_volume
 
 
 #f1 score for pages
@@ -111,7 +134,10 @@ pages_pred = np.squeeze(pages_pred)
 #print 'pages_truth',pages_truth.shape,'pages_pred',pages_pred.shape
 
 f1_pages = f1_score(pages_truth,pages_pred,average='micro')
-print 'pages F1 score',f1_pages
+prescision_pages = precision_score(pages_truth, pages_pred, average='micro') 
+recall_pages = recall_score(pages_truth, pages_pred, average='micro')  
+
+print 'pages prescision', prescision_pages,'recall', recall_pages,'F1 score',f1_pages
 
 
 #f1 score for all labels
@@ -122,5 +148,8 @@ overall_pred = np.squeeze(overall_pred)
 #print 'overall_truth',overall_truth.shape,'overall_pred',overall_pred.shape
 
 f1_overall = f1_score(overall_truth,overall_pred,average='micro')
-print 'overall F1 score',f1_overall
+prescision_overall = precision_score(overall_truth, overall_pred, average='micro') 
+recall_overall = recall_score(overall_truth, overall_pred, average='micro')  
+
+print 'overall prescision', prescision_overall,'recall', recall_overall,'F1 score',f1_overall
 
